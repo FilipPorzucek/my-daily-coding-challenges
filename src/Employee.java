4c3c55f72a6f275b8317f6b6d41c746c99010b07
FilipPorzucek <filipporzucek81@gmail.com>
2024-10-13 20:41:14 +0200

    Zadanie z utworzeniem programu do zapisu i odczytu pracownikow przy uzyciu map

---
+++ src/Employee.java
@@ -0,0 +1,40 @@
+public class Employee {
+    private String name;
+    private String lastName;
+    private int salary;
+
+    public Employee(String name, String lastName, int salary) {
+        this.name = name;
+        this.lastName = lastName;
+        this.salary = salary;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public int getSalary() {
+        return salary;
+    }
+
+    public void setSalary(int salary) {
+        this.salary = salary;
+    }
+
+    @Override
+    public String toString() {
+        return name+" "+lastName+" "+salary;
+    }
+}
