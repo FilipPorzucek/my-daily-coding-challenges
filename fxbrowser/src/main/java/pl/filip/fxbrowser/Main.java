74dcbfafe110a047aa7fa126f12cd7991b8581af
FilipPorzucek <147589167+FilipPorzucek@users.noreply.github.com>
2025-03-09 10:32:20 +0100

    Nauka webView oraz kodu js w javieFX

--- fxbrowser/src/main/java/pl/filip/fxbrowser/Main.java
+++ fxbrowser/src/main/java/pl/filip/fxbrowser/Main.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.web.WebEngine;
 import javafx.scene.web.WebView;
@@ -13,6 +14,10 @@ public class Main extends Application {
 
     @FXML
    private WebView webView;
+    @FXML
+    private Button nextButton;
+    @FXML
+    private Button backButton;
 
     @Override
     public void start(Stage stage) throws Exception {
@@ -30,5 +35,7 @@ public class Main extends Application {
     public void initialize(){
         WebEngine engine = webView.getEngine();
         engine.load("https://google.pl");
+        backButton.setOnAction(event -> engine.executeScript("history.back()"));
+        nextButton.setOnAction(event -> engine.executeScript("history.forward"));
     }
 }
